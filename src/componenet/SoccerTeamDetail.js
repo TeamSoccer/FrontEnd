95b0297e37277e41cabcd893577159f8eae7daa6
ywonchae1 <ywonchae62@gmail.com>
2024-08-23 11:23:03 +0900

    chore: 오류 코드 주석 처리

--- src/componenet/SoccerTeamDetail.js
+++ src/componenet/SoccerTeamDetail.js
@@ -20,13 +20,13 @@ function SoccerTeamDetail() {
       });
   }, [teamIdx]);
 
-  if (!soccerTeam) return <div>Loading...</div>;
+  // if (!soccerTeam) return <div>Loading...</div>;
 
   return (
     <div className="container">
-      <h2>{soccerTeam.teamName}</h2>
+      <h2>{"soccerTeam.teamName"}</h2>
       <form id="frm" method="post">
-        <input type="hidden" id="teamIdx" name="teamIdx" value={soccerTeam.teamIdx} />
+        <input type="hidden" id="teamIdx" name="teamIdx" value={"soccerTeam.teamIdx"} />
         <table className="player_list">
           <colgroup>
             <col width="10%" />
@@ -36,60 +36,60 @@ function SoccerTeamDetail() {
           <tbody>
             <tr>
               <th scope="row">글 번호</th>
-              <td>{soccerTeam.teamIdx}</td>
+              <td>{"soccerTeam.teamIdx"}</td>
               <th scope="row">조회수</th>
-              <td>{soccerTeam.hitCnt}</td>
+              <td>{"soccerTeam.hitCnt"}</td>
               <th scope="row">작성일</th>
-              <td>{new Date(soccerTeam.createdDatetime).toLocaleString()}</td>
+              <td>{new Date("soccerTeam.createdDatetime").toLocaleString()}</td>
               <th scope="row">수정일</th>
-              <td>{new Date(soccerTeam.updatedDatetime).toLocaleString()}</td>
+              <td>{new Date("soccerTeam.updatedDatetime").toLocaleString()}</td>
             </tr>
             <tr>
               <th scope="row">팀 이름</th>
-              <td>{soccerTeam.teamName}</td>
+              <td>{"soccerTeam.teamName"}</td>
               <th scope="row">작성자</th>
-              <td>{soccerTeam.creatorId}</td>
+              <td>{"soccerTeam.creatorId"}</td>
               <th scope="row">연락처</th>
-              <td colSpan="3">{soccerTeam.teamNumber}</td>
+              <td colSpan="3">{"soccerTeam.teamNumber"}</td>
             </tr>
             <tr>
             <th scope="row">요일</th>
-              <td>{soccerTeam.teamDay}</td>
+              <td>{"soccerTeam.teamDay"}</td>
               <th scope="row">진행 시간</th>
-              <td>{soccerTeam.teamTime}시간</td>
+              <td>{"soccerTeam.teamTime"}시간</td>
               <th scope="row">운영 기간</th>
-              <td>{soccerTeam.teamPeriod}년</td>
+              <td>{"soccerTeam.teamPeriod"}년</td>
               <th scope="row">팀 연령대</th>
-              <td>{soccerTeam.teamOld}대</td>
+              <td>{"soccerTeam.teamOld"}대</td>
             </tr>
             <tr>
               <th scope="row">지역</th>
-              <td>{soccerTeam.region}</td>
+              <td>{"soccerTeam.region"}</td>
               <th scope="row">선출 수</th>
-              <td>{soccerTeam.athleteNumber}명</td>
+              <td>{"soccerTeam.athleteNumber"}명</td>
               <th scope="row">필요 포지션</th>
-              <td>{soccerTeam.needPosition}</td>
+              <td>{"soccerTeam.needPosition"}</td>
               <th scope="row">필요 포지션 수</th>
-              <td>{soccerTeam.needPositionNumber}명</td>
+              <td>{"soccerTeam.needPositionNumber"}명</td>
             </tr>
             <tr>
               <th scope="row">제목</th>
-              <td colSpan="7">{soccerTeam.title}</td>
+              <td colSpan="7">{"soccerTeam.title"}</td>
             </tr>
             <tr>
               <th scope="row">내용</th>
-              <td colSpan="7">{soccerTeam.contents}</td>
+              <td colSpan="7">{"soccerTeam.contents"}</td>
             </tr>
           </tbody>
         </table>
       </form>
       <div className="file_list">
         <label>팀 로고 / 팀 홍보물</label>
-        {soccerTeam.fileInfoList && soccerTeam.fileInfoList.map(fileInfo => (
+        {/* {soccerTeam.fileInfoList && soccerTeam.fileInfoList.map(fileInfo => (
           <a key={fileInfo.teamFileIdx} href={`http://localhost:8080/api/soccerTeam/file/${soccerTeam.teamIdx}/${fileInfo.teamFileIdx}`}>
             {fileInfo.originalFileName} ({fileInfo.fileSize}kb)
           </a>
-        ))}
+        ))} */}
       </div>
       <button className="btn" onClick={() => navigate('/')}>목록으로</button>
       <button className="btn" onClick={() => navigate(`/soccerTeamModify/${teamIdx}`)}>수정하기</button>
